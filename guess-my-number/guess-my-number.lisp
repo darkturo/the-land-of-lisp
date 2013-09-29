3857e02bc83dc84a646baf6f9445f6fba2457038
Arturo Escudero <darkturo@gmail.com>
2013-09-30 00:23:49 +0200

    Fixed some more problems.
       * misspelling on the definition of *lower-limit*
       * ash function required one extra parameter, that I missed to add

--- guess-my-number/guess-my-number.lisp
+++ guess-my-number/guess-my-number.lisp
@@ -1,5 +1,6 @@
 (defparameter *upper-limit* 100)
-(defparameter *lower-lmit* 1)
+(defparameter *lower-limit* 1)
 
 (defun guess-my-number ()
-   (ash (+ *upper-limit* *lower-limit*)))
+   (ash (+ *upper-limit* *lower-limit*) -1))
+
