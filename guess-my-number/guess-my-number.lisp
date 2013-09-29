0d2ac1bf070b8f71450423054f383e6bbadbad5a
Arturo Escudero <darkturo@gmail.com>
2013-09-30 00:27:55 +0200

    Added a couple of new functions, bigger and smaller, to play the game.

--- guess-my-number/guess-my-number.lisp
+++ guess-my-number/guess-my-number.lisp
@@ -4,3 +4,13 @@
 (defun guess-my-number ()
    (ash (+ *upper-limit* *lower-limit*) -1))
 
+(defun bigger ()
+   (setf *lower-limit* (1- (guess-my-number))
+   (guess-my-number))
+
+(defun smaller ()
+   (setf *upper-limit* (1+ (guess-my-number))
+   (guess-my-number))
+
+(defun start-over ()
+   )
