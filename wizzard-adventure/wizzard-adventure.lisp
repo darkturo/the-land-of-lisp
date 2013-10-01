72964d33de3b09b3b7ad3044ca6f5ec31f9eae1c
Arturo Escudero <darkturo@gmail.com>
2013-10-01 23:18:52 +0200

    Defined the world using association lists.
    Just typed in the world description using a simple list of lists indexed by
    keyword (an association list). This will give opportunity later to use assoc to
    get retrieve information from this strcuture.

---
+++ wizzard-adventure/wizzard-adventure.lisp
@@ -0,0 +1,7 @@
+;; description of the world
+(defparameter *nodes* '(
+      (living-room (You are in the living-room. A wizzard is snoring loudly on the couch.))
+      (garden (You are in a beatiful garden. There is a well in front of you))
+      (attic (You are in the attic. There is a gigant welding torch in the corner.))))
+
+
