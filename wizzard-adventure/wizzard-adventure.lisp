c65ede82c2b861be286a3653257ca3f7482c99cd
Arturo Escudero <darkturo@gmail.com>
2013-10-01 23:45:11 +0200

    Added new data and a function to determine the options the player can take.
    The function describe-path, is a bit weird, but after observing how it should
    work,  I found that is quite tied to the data it can read from,
    So (describe-path '(garden next door)), after applying the function, will
    return something like: (there is a door going west from here).

--- wizzard-adventure/wizzard-adventure.lisp
+++ wizzard-adventure/wizzard-adventure.lisp
@@ -4,5 +4,15 @@
       (garden (You are in a beatiful garden. There is a well in front of you))
       (attic (You are in the attic. There is a gigant welding torch in the corner.))))
 
+;; edges (definition of the paths available between locations
+(defparameter *edges* '(
+      (living-room (garden west door) (attic upstairs ladder))
+      (garden (living-room east door))
+      (attic (living-room downstairs ladder))))
+
+;; functions for the game
 (defun describe-location (location nodes)
    (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+   '(there is a (caddr edge) going (cadr edge) from here.))
