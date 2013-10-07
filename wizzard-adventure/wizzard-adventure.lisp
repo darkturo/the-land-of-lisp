aed8d8ca4c27cbdf2de9e78484cf84dbaf482406
Arturo Escudero <darkturo@gmail.com>
2013-10-07 04:08:34 +0200

    Printing embelishment :-)
    After adding a couple of functions, the player is able to see the output from
    the program as propper lowercases. In adition to this, the player does not need
    to know which are objects and which are symbols, he just enters texts. Example
    from a run:
    
    [1]> (game-repl)
    look
    You are in the living-room. A wizzard is snoring loudly on the couch. There is a door going west from here. There is a ladder going upstairs from here. You see a whiskey on the
     floor. You see a bucket on the floor.
    walk upstairs
    You are in the attic. There is a gigant welding torch in the corner. There is a ladder going downstairs from here.
    items
    I do not know that command.
    pickup torch
    You cannot get that
    pickup gigant
    You cannot get that
    walk downstairs
    You are in the living-room. A wizzard is snoring loudly on the couch. There is a door going west from here. There is a ladder going upstairs from here. You see a whiskey on the
     floor. You see a bucket on the floor.
    pickup whiskey
    You are now carriying the whiskey
    walk west
    You are in a beatiful garden. There is a well in front of you there is a door going east from here. You see a chain on the floor.
    pickup chain
    You are now carriying the chain
    walk east
    You are in the living-room. A wizzard is snoring loudly on the couch. There is a door going west from here. There is a ladder going upstairs from here. You see a bucket on the
     floor.
    inventory
    Items- whiskey chain
    
    Basically what I've done is to convert what needs to be printed into a string,
    remove its parenthesis, and transform that into a list of characters. That is
    passed to a "list eater" called tweak-text, that is putting the text in upper
    or lower case. Last, the output from tweak-text is coerced back into string,
    and passed to printc, which is printing it in the console.

--- wizzard-adventure/wizzard-adventure.lisp
+++ wizzard-adventure/wizzard-adventure.lisp
@@ -66,10 +66,26 @@
        (eval sexp)
       '(i do not know that command.))) 
 
+(defun tweak-text (lst cap lit)
+   (when lst
+      (let ((item (car lst))
+            (rest (cdr lst)))
+         (cond
+            ((eq item #\space) (cons item (tweak-text rest cap lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eq item #\") (tweak-text rest cap (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            (cap (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
 (defun game-print (lst)
-   (print
-   (string-trim "() " (prin1-to-string lst)))
-   )
+   (princ (coerce (tweak-text (coerce (string-trim "() " 
+                                                   (prin1-to-string lst))
+                                      'list)
+                              t
+                              nil)
+                   'string))
+   (fresh-line))
+         
 
 (defun game-repl ()
    (let ((cmd (game-read)))
