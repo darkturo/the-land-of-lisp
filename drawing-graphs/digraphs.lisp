147eb0c7ab435d35de7ef8c3062a9134a79506e4
Arturo Escudero <darkturo@gmail.com>
2013-10-08 07:13:30 +0200

    Added dot-label
    This function, which is kind of weird, has the purpose of cropping long strings
    into short strings. The idea is not to overload the graph with too much
    information.

--- drawing-graphs/digraphs.lisp
+++ drawing-graphs/digraphs.lisp
@@ -5,3 +5,11 @@
 (defun dot-name (exp)
    (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
 
+(defun dot-label (exp)
+   (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+         (if (> (length s) *max-label-length*)
+            (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+            s))
+      ""))
+
