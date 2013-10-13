dc059500574268fb2bcb4a5d4e985771a59aba6d
Arturo Escudero <darkturo@gmail.com>
2013-10-13 14:30:01 +0200

    Added a bunch of functions to find and link unconnected nodes
    The topology of the graph used as a city, is traced randomly. With this
    technique the possibility of having unnconected nodes is quite hihg, so
    with the functions created, the idea is to track these unconnected nodes
    or islands, and trace edges (or links) with at least one connected node.
    
    The main function, here is connect-all-islands, which uses all the
    auxiliary functions connect-with-bridges, find-islands, get-connected,
    and direct-edges.

--- gran-theft-wupmus/gtw.lisp
+++ gran-theft-wupmus/gtw.lisp
@@ -18,3 +18,43 @@
 (defun make-edge-list ()
   (apply #'append (loop repeat *edge-num* 
                         collect (edge-pair (random-node) (random-node)))))
+
+; get the list of edges that goes from the specified node
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+                   (eql (car x) node))
+                  edge-list))
+
+; get the list of the all the reachable nodes from an specified node 
+(defun get-connected (node edge-list)
+  (let ((visited nil))
+    (labels ((traverse (node)
+               (unless (member node visited)
+                 (push node visited)
+                 (mapc (lambda (edge)
+                         (traverse (cdr edge)))
+                       (direct-edges node edge-list)))))
+         (traverse node))
+      visited))
+
+; find islands, i.e. disconnected nodes
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels ((find-island (nodes)
+               (let* ((connected (get-connected (car nodes edge-list))) 
+                      (unconnected (set-difference nodes connected)))
+                 (push connected islands)
+                 (when unconnected
+                   (find-island unconnected)))))
+         (find-island nodes))
+      islands))
+
+; I lost the track here ... I'm in monkey typing state :-S
+(defun connect-with-bridges (islands)
+  (when (cdr islands)
+    (append (edge-pair (caar islands) (caadr islands)) 
+            (connect-with-bridges (cdr islands)))))
+
+; it will connect-all-islands all the islands found with find-islands
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
