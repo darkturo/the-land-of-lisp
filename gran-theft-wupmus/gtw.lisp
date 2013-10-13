9053adf79826847b2cf6d4032d3223d16d860cc8
Arturo Escudero <darkturo@gmail.com>
2013-10-13 05:47:18 +0200

    Added make-edge-list
    This function creates a list of random interconected nodes. The loop in
    make-edge-list, creates a list of lists of pairs, which is then
    converted just into a list of pairs by means of using apply and append
    functions.

--- gran-theft-wupmus/gtw.lisp
+++ gran-theft-wupmus/gtw.lisp
@@ -10,3 +10,11 @@
 
 (defun random-node ()
   (+ 1 (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num* 
+                        collect (edge-pair (random-node) (random-node)))))
