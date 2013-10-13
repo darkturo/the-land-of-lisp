6c91484653c4a1f8805900dca13c415a164cd4c8
Arturo Escudero <darkturo@gmail.com>
2013-10-13 05:26:52 +0200

    Started with the Gran Theft Wumpus project

---
+++ gran-theft-wupmus/gtw.lisp
@@ -0,0 +1,10 @@
+(load "graph-util.lisp")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cops-odds* 15)
+
